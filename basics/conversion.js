37651cb1b4e29d40478b59ad99611dca35632b44
Piyush Jeswani <30585789+Piyush-Jeswani@users.noreply.github.com>
2023-10-12 09:27:44 +0000

    conversion file

---
+++ basics/conversion.js
@@ -0,0 +1,68 @@
+let score = 20
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueIn = Number(score)
+
+console.log(typeof valueIn);
+console.log(valueIn);
+
+console.log("----------------------");
+
+
+ score = "20"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+ valueIn = Number(score)
+
+console.log(typeof valueIn);
+console.log(valueIn);
+console.log("----------------------");
+
+score = "20ab"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+ valueIn = Number(score)
+
+console.log(typeof valueIn);
+console.log(valueIn);
+
+console.log("----------------------");
+
+score = undefined
+
+console.log(typeof score);
+console.log(typeof (score));
+
+ valueIn = Number(score)
+
+console.log(typeof valueIn);
+console.log(valueIn);
+
+console.log("----------------------");
+
+score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+ valueIn = Number(score)
+
+console.log(typeof valueIn);
+console.log(valueIn);
+
+console.log("----------------------");
+
+score = 1
+
+console.log(typeof score);
+
+ valueIn = Boolean(score)
+
+console.log(typeof valueIn);
+console.log(valueIn);
