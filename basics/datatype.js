bb4b3ed043c219fd637d7fcbcd9a1a55515be6d4
Piyush Jeswani <30585789+Piyush-Jeswani@users.noreply.github.com>
2023-10-12 07:33:44 +0000

    adding datatype

---
+++ basics/datatype.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log("piyush"); console.log("jeswani")
+
+// number 
+// bigint
+//string
+//boolean
+//null
+//undefined
+//symbol
+
+console.log(typeof "piyush");
+console.log(typeof undefined);
---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const bankAccountNumber = "abc001";
+
+var accountName = "Piyush"
+let accountEmail = "piyush@somewhere.com";
+let accountState;
+
+// bankAccountNumber = "efg002"
+
+accountName ="Jeswani"
+
+console.table([bankAccountNumber, accountName, accountEmail, accountState])
